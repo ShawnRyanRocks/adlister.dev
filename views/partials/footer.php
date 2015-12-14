0eda19cdb6a9847d705f04dfbcbec169dd65328b
Shawn Pivonka <shawnpivonka@Shawns-MacBook-Pro.local>
2015-12-14 16:10:17 -0600

    Added Footer and link to css in the header.  Look out Ryan im going to give you a conflict. Still need to add some mobile responsiveness to the footer.

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,8 +1,29 @@
     </main>
+
     <footer>
-        <ul>
-            <li>C 2015 Ryan&Shawn</li>
-        </ul>
+
+        <div class="col-sm-5">
+            <ul class="footer_ul_class" id="footer_ul_left">
+                <li class="footer_li_class"><a href="#">FAQ's</a></li>
+                <li class="footer_li_class"><a href="#">Feedback</a></li>
+                <li class="footer_li_class"><a href="#">Terms and Conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-2">
+            <img src="/img/codeup-arrow.png">
+        </div>
+
+        <div class="col-sm-5">
+            <ul class="footer_ul_class" id="footer_ul_right">
+                <li class="footer_li_class"><a href="#">another link</a></li>
+                <li class="footer_li_class"><a href="#">last link</a></li>
+                <li class="footer_li_class"><a href="#">And one more link</a></li>
+            </ul>
+        </div>
+
+        <div id="footer_incoparated_info">C 2015 Ryan&Shawn</div>
+
     </footer>
 </body>
 </html>
