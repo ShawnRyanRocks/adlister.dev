8d2517366601459dd644501b25abb23ca3e20444
Ryan Bentkowski <ryanbentkowski@gmail.com>
2015-12-14 16:59:42 -0600

    Finished Navbar for now.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,45 +1,27 @@
-<style type="text/css">
-input {
-	float: left;
-	color: red;
-	
-}
 
-label {
-	float: left;
-	color: red;
-	
-}
 
-ul {
-	list-style-type: none;
-}
 
-h3 {
-	float: center;
-	color: red; 
-}
-
-</style>
-<div class="col-md-4">
-	<div class="search">
-		<ul>
-			<li><label for "search">Search</label></li>
-			<li><input type="text" id="search" name='search'></li>
-		</ul>	
+<div class='row'>
+	<div class="col-md-4">
+		<div class="search">
+			<ul>
+				<li><label for "search">Search</label></li>
+				<li><input type="text" id="search" name='search'></li>
+			</ul>	
+		</div>
 	</div>
-</div>
 
-<div class="col-md-4">
-	<div>
-		
+	<div class="col-md-4">
+		<div>
+			
 
+		</div>
 	</div>
-</div>
 
-<div class="col-md-4">
-	<div>
-		<a href='#'>Login</a>
+	<div class="col-md-4">
+		<div>
+			<a href='#'>Login</a>
+		</div>
 	</div>
 </div>
 
