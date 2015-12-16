b6024ee8783a77a1e6ba5e22264228c3ab397d87
William Swantner <pswantner@gmail.com>
2015-12-16 11:46:49 -0600

    adding my example toy page, with image, also an updated main js, that changed formating a little bit for my page to follow theme.

---
+++ public/ads.show.php
@@ -0,0 +1,40 @@
+<?php include '../views/partials/header.php'; ?>
+
+<?php include '../views/partials/navbar.php'; ?>
+
+
+ 	<div class="hidden-xs col-sm-2  col-md-2 col-lg-3">
+        <?php include '../views/partials/near.by.cities.php'; ?>
+    </div>
+<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
+	<div class ='main_body'>
+		<div id='category'>
+			<div id='title'><h3> Toy - For Sale by Owner Pickup @ Orphanage</h3></div>
+			<div id='price'><h4>$20</h4></div>
+			<div id='zipcode'><h4>78242</h4></div>
+			<div id='posting.title'><h4>Like New Boardgame Mousetrap</h4>
+			</div>
+		</div>
+
+
+
+	</div>
+		<div id='details'>
+			<div id='image' class = 'center'><img class = 'img-responsive img-center' src='/img/mousetrap.jpg'/></div>
+		</div>
+
+	<h5>Description</h5>
+	<p>Board Game hardly used, only one trap functional, so its almost never been touched. Twenty Dollars!</p>
+</div>
+
+	<div class="hidden-xs hidden-sm col-md-2 col-lg-3">
+        <?php include '../views/partials/desktop.ads.php'; ?>
+    </div>
+    
+    <div class="col-xs-12 col-sm-12 hidden-md hidden-lg">
+                    <?php include '../views/partials/mobile.ads.php'; ?>
+    </div>
+            
+
+
+<?php include '../views/partials/footer.php'; ?>
