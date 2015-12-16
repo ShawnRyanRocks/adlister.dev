463c8197e1b99a66802de4deff18d086b6da75c8
William Swantner <pswantner@gmail.com>
2015-12-16 12:14:44 -0600

    just pushing it on its a dummy page for truck, im moving on to the indexes that ryan just pushed and likning everything together. this is a truck add just for shawn, though these adds are not set up to be dynamically loaded so its just a dummy page.

---
+++ public/ads.show.truck.php
@@ -0,0 +1,40 @@
+<?php include '../views/partials/header.php'; ?>
+
+<?php include '../views/partials/navbar.php'; ?>
+
+
+ 	<div class="hidden-xs col-sm-2  col-md-2 col-lg-3">
+        <?php include '../views/partials/near.by.cities.php'; ?>
+    </div>
+<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
+	<div class ='main_body'>
+		<div id='category'>
+			<div id='title'><h3> Truck!! - For Sale by Owner Pickup @ Shawn</h3></div>
+			<div id='price'><h4>Open to interesting trades as long as its anothere truck</h4></div>
+			<div id='zipcode'><h4>78242</h4></div>
+			<div id='posting.title'><h4>Like New Truck</h4>
+			</div>
+		</div>
+
+
+
+	</div>
+		<div id='details'>
+			<div id='image' class = 'center'><img class = 'img-responsive img-center' src='/img/truck.jpeg'/></div>
+		</div>
+
+	<h5>Description</h5>
+	<p>I havent mentiotned this but i love trucks!Looking to trade trucks!!</o>
+</div>
+
+	<div class="hidden-xs hidden-sm col-md-2 col-lg-3">
+        <?php include '../views/partials/desktop.ads.php'; ?>
+    </div>
+    
+    <div class="col-xs-12 col-sm-12 hidden-md hidden-lg">
+                    <?php include '../views/partials/mobile.ads.php'; ?>
+    </div>
+            
+
+
+<?php include '../views/partials/footer.php'; ?>
