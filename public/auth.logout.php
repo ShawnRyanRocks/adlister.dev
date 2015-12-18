6aa5af51110539cc1da562f04bbb1f6736b15d05
Ryan Bentkowski <ryanbentkowski@gmail.com>
2015-12-18 11:48:34 -0600

    added bootstrap file correctly and called logout function so logout works correctly.

--- public/auth.logout.php
+++ public/auth.logout.php
@@ -1,30 +1,12 @@
 <?php include_once '../bootstrap.php'; ?>
 
-<?php include '../views/partials/header.php'; ?>
+Auth::logout();
 
-<div class="row">
-<?php include_once '../views/partials/navbar.php'; ?>
-<?php bootstrap.php ?>
+
 <?php 
+	Auth::logout();
+
 	header('location:index.php');
-session_destroy();
+	die();
 
 ?>
-</div>
-
-<body>
-
-	<div class="row">
-		<h3>You are logged out.</h3>
-	</div>
-	
-	<div class="row">
-			<a href="index.php">Back</a>
-	</div>
-</div>
-
-	
-
-	</body>
-
-	<?php include '../views/partials/footer.php'; ?>
