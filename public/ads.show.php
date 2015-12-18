47c0899c24d34f4ac46ae65630f8a41d7ae40694
Ryan Bentkowski <ryanbentkowski@gmail.com>
2015-12-18 11:12:19 -0600

    not good changes.

--- public/ads.show.php
+++ public/ads.show.php
@@ -1,38 +1,35 @@
 <?php include '../views/partials/header.php'; ?>
 
 <?php include '../views/partials/navbar.php'; ?>
+
 <div class='row'>
 	<h3>For Sale</h3>
 </div>
 
- 	<div class="hidden-xs col-sm-2  col-md-2 col-lg-3">
+ 	<div class="hidden-xs col-sm-2  col-md-2 col-lg-2">
         <?php include '../views/partials/nearby.cities.php'; ?>
     </div>
-<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
-	<div class ='main_body'>
-		<div id='category'>
-			<div id='title'><h3> Toy - For Sale by Owner Pickup @ Orphanage</h3></div>
-			<div id='price'><h4>$20</h4></div>
-			<div id='zipcode'><h4>78242</h4></div>
-			<div id='posting.title'><h4>Like New Boardgame Mousetrap</h4>
-			</div>
-		</div>
-
-
-
-	</div>
-		<div class='col-xs-12 col-sm-6'>
+<div class="col-xs-12 col-sm-10 col-md-8 col-lg-8">
+	<div class="row">
+		<!-- <div class='col-xs-12 col-sm-6'> -->
 			<div id='details'>
-				<div id='image' class = 'center'><img class = 'img-responsive img-center' src='/img/mousetrap.jpg'/></div>
+				<div id='image' class = 'center'><img class = 'img-center' src='/img/mousetrap.jpg'/></div>
+		<div id='category'>
+				<div id='title'>
+					<h3> Toy - For Sale by Owner Pickup @ Orphanage</h3></div>
+				<div id='price'><h5>$20</h5></div>
+				<div id='zipcode'><h5>78242</h5></div>
+				<div id='posting.title'><h5>Like New Boardgame Mousetrap</h5>
+					<h5>Description</h5>
+					<p>Board Game hardly used, only one trap functional, so its almost never been touched. Twenty Dollars!</p>
+				</div>
 			</div>
 		</div>
-	<div class='col-xs-12 col-sm-6'>
-		<h5>Description</h5>
-		<p>Board Game hardly used, only one trap functional, so its almost never been touched. Twenty Dollars!</p>
-	</div>
+	<!-- </div> -->
+</div>
 </div>
 
-	<div class="hidden-xs hidden-sm col-md-2 col-lg-3">
+	<div class="hidden-xs hidden-sm col-md-2 col-lg-2">
         <?php include '../views/partials/desktop.ads.php'; ?>
     </div>
     
