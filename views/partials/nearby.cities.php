7076d156e1df3ac71057aefd5f7e460fef6b2645
Shawn Pivonka <shawnpivonka@shawns-mbp.gateway.pace.com>
2015-12-28 09:52:42 -0600

    Got nearby cities working, having issues with using models with the nearby cities file since they are included in a pagecontroller on the top of the pages that include the nearby.cities.php.  To work around this i hard coded code that was needed instead of requireing bootstrap

--- views/partials/nearby.cities.php
+++ views/partials/nearby.cities.php
@@ -1,33 +1,71 @@
 <?php
-      include_once '../bootstrap.php';
-?>
+// require '../../bootstrap.php';
+// require '../../database/db.connect.php'; 
 
-<div class="col-md-4">
-        <div class="cities_nearby">Nearby Cities
+// Get new instance of PDO object
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// if(Auth::check())
+if (isset($_SESSION['LOGGED_IN']) ? $_SESSION['LOGGED_IN'] : false)
+{ 
+    $query = "
+        SELECT `longitude`,`latitude`
+        FROM `location`
+        WHERE `locId` = :locId
+    ";
+    $stmt = $dbc->prepare($query);
+    $stmt->bindValue(":locId",$_SESSION['LOGGED_IN_USER']->locId,PDO::PARAM_INT);
+    $stmt->execute();
+} else {
+    $query = "
+        SELECT `longitude`,`latitude`
+        FROM `location`
+        WHERE `postalCode` = 78250
+    ";
+    $stmt = $dbc->prepare($query);
+    $stmt->execute();
+}
+
+    $geo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $lon = $geo['longitude'];
+    $lat = $geo['latitude'];
     
 
-            <ul class="cities_nearby">
-                  <li><a href="">Abilene</a></li>
-                  <li><a href ="">Austin</a></li>
-                  <li><a href ="">Beauomont</a></li>
-                  <li><a href ="">Brownsvile</a></li>
-                  <li><a href ="">College Station</a></li>
-                  <li><a href ="">Corpus Christi</a></li>
-                  <li><a href =''>Dallas</a></li>
-                  <li><a href =''>Del Rio</a></li>
-                  <li><a href =''>Galveston</a></li>
-                  <li><a href = ''>Houston</a></li>
-                  <li><a href =''>Kileen</a></li>
-                  <li><a href =''>Laredo</a></li>
-                  <li><a href =''>Lubbock</a></li>
-                  <li><a href =''>Mcallen</a></li>
-                  <li><a href = ''>Odessa</a></li>
-                  <li><a href =''>Nacodoches</a></li>
-                  <li><a href =''>San Marcos</a></li>
-                  <li><a href =''>Texarkana</a></li>
-                  <li><a href = ''>Victoria</a></li>
-                  <li><a href = ''>Waco</a></li>
-            </ul>
+    $query = "
+        SELECT `locId`, `city`, ( 3959 * acos( cos( radians(:lat) ) * cos( radians( latitude ) ) 
+        * cos( radians( longitude ) - radians(:lon) ) + sin( radians(:lat) ) * sin(radians(latitude)) ) ) AS `distance` 
+        FROM `location`
+        HAVING `distance` < 50
+        ORDER BY `distance`
+        LIMIT 0,300;
+    ";
+    $stmt = $dbc->prepare($query);
+    $stmt->bindValue(":lat", $lat, PDO::PARAM_INT);
+    $stmt->bindValue(":lon", $lon, PDO::PARAM_INT);
+    $stmt->execute();
 
-      </div>
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $cities = [];
+    $used = [];
+    foreach($results as $city)
+    {
+        if(!in_array($city['city'],$used) && count($used) < 20)
+        {
+              $cities[] = $city;
+              $used[] = $city['city'];
+        }
+    }
+?>
+<div class="col-sm-12 clearmargin" id="nearby_cities_positioning_div">
+    <ul id="nearby_cities_unorder_list">
+        <?php foreach($cities as $city) : ?>
+            <li><a href="ads.show.php?city=<?=$city['locId'];?>"><?= $city['city']; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
 </div>
+ 
