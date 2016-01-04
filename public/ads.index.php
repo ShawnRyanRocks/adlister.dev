6e1a648d93e3a865758d9b14e08f7df7034b7e4d
William Swantner <pswantner@gmail.com>
2016-01-04 09:32:09 -0600

    got the right one from github and ryan

--- public/ads.index.php
+++ public/ads.index.php
@@ -1,46 +1,33 @@
 <?php 
 include_once '../bootstrap.php'; 
-
-  $stmt1 = $dbc->query('SELECT * FROM posts');
-  $posts = $stmt1->fetchAll(PDO::FETCH_ASSOC);
- 
-
-
-$category = Input::get('category');
-
-
-// $query = "SELECT * FROM `posts` WHERE category = :category";
-// $stmt = $dbc->prepare($query);
+ $stmt = $dbc->query('SELECT * FROM posts');
+ $posts=$stmt->fetchAll(PDO::FETCH_ASSOC);
+ if( Input::has('search') )
+ {
+  $search = Input::get('search');
+  $search = trim($search);
+  $results = Post::findBySearch($search);
+  // $search = Input::get('search');
+  // $query=
+  // "
+  //   SELECT * FROM `posts` 
+  //   WHERE `title` 
+  //     LIKE '%:search%' 
+  //   OR `description` 
+  //     LIKE '%:search%' 
+  //   OR `category` 
+  //     LIKE '%:search%';
+  // "; 
+  // $stmt = $dbc->prepare($query);
+  // $stmt->bindValue(':search', $search, PDO::PARAM_STR);
+  // $stmt->execute();
+  // $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($stmt);
+echo $search;
+var_dump($results);
+ }
   
-// $stmt->bindValue(":category", $category, PDO::PARAM_STR);
-
-// $stmt->execute();
-
-// determine page number from $_GET
-$page = 1;
-if(!empty($_GET['page'])) {
-    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-    if(false === $page) {
-        $page = 1;
-    }
-}
-
-
-// set the number of items to display per page
-$items_per_page = 4;
-
-// build query
-$offset = ($page - 1) * $items_per_page;
-$sql = "SELECT * FROM posts LIMIT " . $offset . "," . $items_per_page;
-
-
-$stmt = $dbc->prepare($sql);
   
-$stmt->bindValue(":category", $category, PDO::PARAM_STR);
-
-$stmt->execute();
-
-
 ?>
 <?php include '../views/partials/header.php'; ?>
 
@@ -59,33 +46,31 @@ $stmt->execute();
 
    
 
-		
+    
 
-			<?php foreach($posts as $post): ?>
-				<div class="imgAdIndex">
-  					<a  href="http://adlister.dev/ads.show.php">
-    				<img src="/img/mousetrap.jpg" alt="" width="250" height="250">
-  					<div class="descAdIndex"><?= $post['title']?>(<?= $post['zip']?>)</div>
-					</a>
-				</div>
+  <!--    <?php foreach($results as $result): ?>
+        <div class="img">
+            <a  href="http://adlister.dev/ads.show.php">
+            <img src="/img/mousetrap.jpg" alt="" width="250" height="250">
+            <div class="desc"><?= $result->title;?></div>
+          </a>
+        </div>
+      <?php endforeach;?> -->
 
-
-			<?php endforeach;?>
-
-	
+  
 
       </div>
     </div>
 
           
-    
-	
+      <ul>
+  
                       
            
 
              <div class="hidden-xs hidden-sm col-md-2 col-lg-2">
-        		<?php include '../views/partials/desktop.ads.php'; ?>
-		    </div>           
+            <?php include '../views/partials/desktop.ads.php'; ?>
+        </div>           
 
 
            
@@ -98,9 +83,11 @@ $stmt->execute();
 
 
     </div>
-	</div>
+  </div>
  </div>
 </div>
 <div id='footer'>
 <?php include '../views/partials/footer.php'; ?>
 </div>
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
